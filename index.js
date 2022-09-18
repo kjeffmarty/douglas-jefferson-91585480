af623ea858feada1154a427a7d7961f7062e73c9
kjeffmarty <112226659+kjeffmarty@users.noreply.github.com>
2022-09-18 01:41:57 -0300

    initial files

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.get('/api/info/getCustomer', (req,res) => {
 
 
 app.get('/api/user/profile', (req,res) => { 
-    res.json({"code":1,"msg":"OK","time":1663467902,"data":{"username":"Douglas","invite_code":"120297","headpic":"\/static_new6\/headimg\/151.png","balance":"10.000","freeze_balance":"0.000","tel":"douglasjefferson9636@gmail.com","today_unsettled":"0.0000","today_settled":"0.00","deal_count":0,"id":120297,"id_status":0,"real_name":"","id_card_num":"","txphone":"","disable_model":[],"task":"30","phone":null,"bankInfo":null,"share_url":"https:\/\/www.nassshop.com\/#\/login\/register?code=120297","otc_member_level":0}})
+    res.json({"code":1,"msg":"OK","time":1663467902,"data":{"username":"Douglas","invite_code":"120297","headpic":"\/static_new6\/headimg\/151.png","balance":"9000.000","freeze_balance":"0.000","tel":"douglasjefferson9636@gmail.com","today_unsettled":"0.0000","today_settled":"0.00","deal_count":0,"id":120297,"id_status":0,"real_name":"","id_card_num":"","txphone":"","disable_model":[],"task":"30","phone":null,"bankInfo":null,"share_url":"https:\/\/www.nassshop.com\/#\/login\/register?code=120297","otc_member_level":0}})
  })
 
 
