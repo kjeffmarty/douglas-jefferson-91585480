774559781ccfa418ba2c8040f541515b4b53ebe6
kjeffmarty <112226659+kjeffmarty@users.noreply.github.com>
2022-09-23 05:35:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: "https://http://127.0.0.1:5500",
+    origin: "http://127.0.0.1:5500",
     methods: ["GET", "POST", ]
  }))
  
