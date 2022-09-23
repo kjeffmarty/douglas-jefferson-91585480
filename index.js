241c1829cd189b56173084c5f6bd2bb4ac669fd3
kjeffmarty <112226659+kjeffmarty@users.noreply.github.com>
2022-09-23 00:33:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 
-const port = process.env.PORT || 3000;
-
-
-
-
-
-app.use(cors({
-    origin: "https://dodomartin966.netlify.app",
-    methods: ["GET", "POST", "PATCH","PUT", "DELETE"]
- }))
- 
 
 
  
