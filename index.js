2fd680463f699784346ae1059228a417c83034d7
kjeffmarty <112226659+kjeffmarty@users.noreply.github.com>
2022-09-23 23:17:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: "https://dodomartin966.netlify.appp",
+    origin: "https://dodomartin966.netlify.app",
     methods: ["GET", "POST", ]
  }))
  
