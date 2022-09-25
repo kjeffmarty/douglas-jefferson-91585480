7fdcefc54eb3e980260f16601eb8efcddb01cb68
kjeffmarty <112226659+kjeffmarty@users.noreply.github.com>
2022-09-25 16:16:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-app.ise(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }))
 
 const port = process.env.PORT || 3000;
 
